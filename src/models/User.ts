f544eb202bbcece4041536a4f23e7bec28463708
Yan Soares <yts.1995@gmail.com>
2020-04-16 20:32:28 -0300

    feat: add timestamps columns

--- src/models/User.ts
+++ src/models/User.ts
@@ -6,13 +6,19 @@ import {
     UpdateDateColumn,
 } from 'typeorm';
 
-@Entity('appointments')
-class Appointment {
+@Entity('users')
+class User {
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
     @Column()
-    provider: string;
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
 
     @CreateDateColumn()
     created_at: Date;
@@ -21,4 +27,4 @@ class Appointment {
     updated_at: Date;
 }
 
-export default Appointment;
+export default User;
--- src/server.ts
+++ src/server.ts
@@ -11,5 +11,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
-    console.log('Server started on port 3333');
+    console.log('🚀 Server started on port 3333!');
 });
