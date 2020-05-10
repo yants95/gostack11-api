f1a932c855ca1c85e47935f15a89a9a8c0ceecbb
Yan Soares <yts.1995@gmail.com>
2020-05-10 16:48:28 -0300

    test: one more test added

--- src/modules/users/services/UpdateProfileService.spec.ts
+++ src/modules/users/services/UpdateProfileService.spec.ts
@@ -36,6 +36,16 @@ describe('UpdateProfile', () => {
         expect(updatedUser.email).toBe('johntre@example.com');
     });
 
+    it('should not be able to update the profile from non existing user', async () => {
+        await expect(
+            updateProfile.execute({
+                user_id: 'non-existing-user-id',
+                name: 'Test',
+                email: 'test@example.com',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+
     it('should be able to change email to another user email', async () => {
         await fakeUsersRepository.create({
             name: 'John Doe',
