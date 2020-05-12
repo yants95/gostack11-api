70cfbe1476f64e38a88ca12225e5cf9b9d6b2f42
Yan Soares <yts.1995@gmail.com>
2020-05-11 22:21:55 -0300

    refactor: pass user_id to CreateAppointmentService test file

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -16,6 +16,7 @@ describe('CreateAppointment', () => {
     it('should be able to create a new appointment', async () => {
         const appointment = await createAppointment.execute({
             date: new Date(),
+            user_id: '123123',
             provider_id: '123123',
         });
 
@@ -29,12 +30,14 @@ describe('CreateAppointment', () => {
         await createAppointment.execute({
             date: appointmentDate,
             provider_id: '123123',
+            user_id: '123123',
         });
 
         expect(
             createAppointment.execute({
                 date: appointmentDate,
                 provider_id: '123123',
+                user_id: '123123',
             }),
         ).rejects.toBeInstanceOf(AppError);
     });
