3b30bfb1e86c9b7e6c54f5de1494bc670d40f70c
Yan Soares <yts.1995@gmail.com>
2020-05-11 21:19:52 -0300

    feat: findyAllInMonthFromProvider method added to interface IAppointmentsRepository

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,7 +1,11 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
 import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
+import IFindAllInMonthFromProviderDTO from '../dtos/IFindAllInMonthFromProviderDTO';
 
 export default interface IAppointmentsRepository {
     create(data: ICreateAppointmentDTO): Promise<Appointment>;
     findByDate(date: Date): Promise<Appointment | undefined>;
+    findyAllInMonthFromProvider(
+        data: IFindAllInMonthFromProviderDTO,
+    ): Promise<Appointment[]>;
 }
