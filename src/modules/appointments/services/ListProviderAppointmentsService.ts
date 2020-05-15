a9b1f16ba9d547008f4a7eede902df488d8c7cba
Yan Soares <yts.1995@gmail.com>
2020-05-15 17:25:11 -0300

    feat: implemented cache for providers appointments

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -27,20 +27,24 @@ class ListProviderAppointmentsService {
         year,
         day,
     }: IRequest): Promise<Appointment[]> {
-        const cacheData = await this.cacheProvider.recovery('asd');
+        const cacheKey = `provider-appointments:${provider_id}-${year}-${month}-${day}`;
 
-        console.log(cacheData);
-
-        const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
-            {
-                provider_id,
-                month,
-                year,
-                day,
-            },
+        let appointments = await this.cacheProvider.recover<Appointment[]>(
+            cacheKey,
         );
 
-        // await this.cacheProvider.save('asd', 'asd');
+        if (!appointments) {
+            appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+                {
+                    provider_id,
+                    month,
+                    year,
+                    day,
+                },
+            );
+
+            await this.cacheProvider.save(cacheKey, appointments);
+        }
 
         return appointments;
     }
