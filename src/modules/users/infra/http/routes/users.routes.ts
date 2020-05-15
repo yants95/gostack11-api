82e0afd0bf72dc5655d991b3a1750368b6bb7eb4
Yan Soares <yts.1995@gmail.com>
2020-05-15 15:18:31 -0300

    refactor: file changed to use new uploadConfig setting

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -11,7 +11,8 @@ import UserAvatarController from '../controllers/UserAvatarController';
 const usersRouter = Router();
 const usersController = new UsersController();
 const userAvatarController = new UserAvatarController();
-const upload = multer(uploadConfig);
+
+const upload = multer(uploadConfig.multer);
 
 usersRouter.post(
     '/',
