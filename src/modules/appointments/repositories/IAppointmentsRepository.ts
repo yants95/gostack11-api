c17340485f3f93df18a0456ec07a3f383261634d
Yan Soares <yts.1995@gmail.com>
2020-05-22 10:16:45 -0300

    fix: provider_id param added

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -5,7 +5,10 @@ import IFindAllInDayFromProviderDTO from '../dtos/IFindAllInDayFromProviderDTO';
 
 export default interface IAppointmentsRepository {
     create(data: ICreateAppointmentDTO): Promise<Appointment>;
-    findByDate(date: Date): Promise<Appointment | undefined>;
+    findByDate(
+        date: Date,
+        provider_id: string,
+    ): Promise<Appointment | undefined>;
     findyAllInMonthFromProvider(
         data: IFindAllInMonthFromProviderDTO,
     ): Promise<Appointment[]>;
