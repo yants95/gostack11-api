82c880b2426009ec5f466b648b07327af8fadbcb
Yan Soares <yts.1995@gmail.com>
2020-06-13 13:07:35 -0300

    feat: add classToClass

--- src/modules/appointments/infra/http/controllers/ProvidersController.ts
+++ src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import ListProvidersService from '@modules/appointments/services/ListProvidersService';
+import { classToClass } from 'class-transformer';
 
 export default class ProvidersController {
     public async index(
@@ -16,6 +17,6 @@ export default class ProvidersController {
             user_id,
         });
 
-        return response.json(providers);
+        return response.json(classToClass(providers));
     }
 }
